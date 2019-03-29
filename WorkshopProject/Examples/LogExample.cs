cc3481a0e00e86a755a201c2b878583240197f95
Unknown <wolfj@post.bgu.ac.il>
2019-03-29 22:02:21 +0300

    added Log with log example

---
+++ WorkshopProject/Examples/LogExample.cs
@@ -0,0 +1,18 @@
+﻿using log4net;
+using System;
+
+namespace WorkshopProject.Examples
+{
+    class LogExample
+    {
+        private static ILog myLogger = LogManager.GetLogger("file");
+
+        public static void RunMe()
+        {
+           // Console.ReadLine();
+            myLogger.Info("somethingnewnew");
+            
+        }
+
+    }
+}
--- WorkshopProject/Program.cs
+++ WorkshopProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorkshopProject.Examples;
 
 namespace WorkshopProject
 {
@@ -10,6 +11,15 @@ namespace WorkshopProject
     {
         static void Main(string[] args)
         {
+            
+            Examples();
         }
+
+
+        static void Examples()
+        {
+            LogExample.RunMe();
+        }
+        
     }
 }
