775de6a35531f9201518591b570c3855e8487bf6
Unknown <wolfj@post.bgu.ac.il>
2019-03-29 19:54:50 +0300

    added example unit test

---
+++ TestingFramework/UnitTests/ClassForTestExampleTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorkshopProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopProject.Tests
+{
+    [TestClass()]
+    public class ClassForTestExampleTests
+    {
+        [TestMethod()]
+        [TestCategory("Example")]
+        public void TestMeTest()
+        {
+            Assert.IsTrue(ClassForTestExample.TestMe());
+        }
+    }
+}
