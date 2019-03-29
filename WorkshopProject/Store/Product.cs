6867bec2d5902d1c8f75160bd6f6617d229ae78d
ג <>
2019-03-29 15:39:47 +0300

    add all method sig
    add all method sig to -Store, StroeManager,Product

--- WorkshopProject/Store/Product.cs
+++ WorkshopProject/Store/Product.cs
@@ -8,5 +8,24 @@ namespace WorkshopProject
 {
     class Product
     {
+        int id;
+        string name;
+        int price;
+        string category;
+        int rank;
+
+        public Product(int id, string name , int price,string category,int rank)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.category = category;
+            this.rank = rank;
+        }
+
+        List<Product> search(string name,string category,int priceRange,int ranking, int storeRanking)
+        {
+            return new List<Product>();
+        }
     }
 }
--- WorkshopProject/Store/Store.cs
+++ WorkshopProject/Store/Store.cs
@@ -8,5 +8,61 @@ namespace WorkshopProject
 {
     class Store
     {
+
+         string name;
+         int rank;
+         Boolean isActive;
+
+        Dictionary<int, Product> products;
+
+        public Store()
+        {
+            products = new Dictionary<int, Product>();
+
+        }
+
+        Store createNewStore(User StoreOwner)
+        {
+            return this;
+        }
+
+        Boolean addProduct(User user,Product product)
+        {
+            return true;
+        }
+
+        Boolean removeProduct(User user, Product product)
+        {
+            return true;
+        }
+
+        Boolean addDiscount(User user, DiscountPolicy discount)
+        {
+            return true;
+        }
+
+        Boolean removeDiscount(User user, DiscountPolicy discount)
+        {
+            return true;
+        }
+
+        Boolean addPurchasingPolicy(PurchasePolicy pPolicy)
+        {
+            return true;
+        }
+
+        Boolean removePurchasingPolicy(PurchasePolicy pPolicy)
+        {
+            return true;
+        }
+
+        Boolean addStoremanager(User user, User storeManager)
+        {
+            return true;
+        }
+
+
+
+
     }
 }
