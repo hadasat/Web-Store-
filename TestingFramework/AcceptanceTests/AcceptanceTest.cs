a30f216db46e9d755557f688facc1e3d962977a2
Unknown <wolfj@post.bgu.ac.il>
2019-03-30 11:45:32 +0300

    added bridge interface and proxy

---
+++ TestingFramework/AcceptanceTests/AcceptanceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingFramework.AcceptanceTests
+{
+    [TestClass]
+    public class AcceptanceTest
+    {
+        private IServiceBridge bridge;
+
+        [TestInitialize]
+        virtual public void init()
+        {
+            bridge = Driver.getBridge();
+        }
+
+
+        [TestCleanup]
+        virtual public void cleanup()
+        {
+
+        }
+
+    }
+}
