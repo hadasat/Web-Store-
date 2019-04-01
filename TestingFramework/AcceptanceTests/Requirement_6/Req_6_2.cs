f568a892b1afd18cbac8b35b0ec60b77208e5513
Unknown <wolfj@post.bgu.ac.il>
2019-04-01 19:35:06 +0300

    6.2

--- TestingFramework/AcceptanceTests/Requirement_6/Req_6_2.cs
+++ TestingFramework/AcceptanceTests/Requirement_6/Req_6_2.cs
@@ -9,20 +9,58 @@ namespace TestingFramework.AcceptanceTests.Requirement_1
         [TestInitialize]
         public override void Init()
         {
-
+            addTestMemberToSystem();
+            addTestStoreOwner1ToSystem();
         }
 
         [TestCleanup]
         public override void Cleanup()
         {
-
+            removeTestStoreOwner1FromSystem();
+            removeTestMemberFromSystem();
         }
 
         [TestMethod]
         [TestCategory("Req_6")]
-        public void InitializeWithAdmin()
+        public void RemoveNormalMemberSuccess()
         {
-            //TODO: test 6.2
+            bool result = bridge.RemoveUser(user);
+            Assert.IsTrue(result);
+
+            result = bridge.Login(user, password);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        [TestCategory("Req_6")]
+        public void RemoveNormalMemberThatWasAlreadyRemoved()
+        {
+            RemoveNormalMemberSuccess();
+
+            bool result = bridge.RemoveUser(user);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        [TestCategory("Req_6")]
+        public void RemoveNormalMemberIllegal()
+        {
+            bool result = bridge.RemoveUser(";");
+            Assert.IsFalse(result);
+        }
+
+
+        [TestMethod]
+        [TestCategory("Req_6")]
+        public void RemoveSoleStoreOwnerSuccess()
+        {
+            bool result = bridge.RemoveUser(storeOwner1);
+            Assert.IsTrue(result);
+
+            result = bridge.Login(user, password);
+            Assert.IsFalse(result);
+
+            //TODO: verify that store doesnt exist when this feature is added
         }
     }
 }
