f3213a4ba29e59f7002f7bc417deacf140a05a2e
Unknown <wolfj@post.bgu.ac.il>
2019-04-09 19:05:56 +0300

    ...

--- TestingFramework/UnitTests/Purchasing UnitTest/ShoppingCartTest.cs
+++ TestingFramework/UnitTests/Purchasing UnitTest/ShoppingCartTest.cs
@@ -14,7 +14,7 @@ namespace WorkshopProject.Tests
         int[] amount = { 10, 20, 30, 40 };
         Product p1, p2, p3, p4;
 
-        [TestInitialize]
+        //[TestInitialize]
         public void Init()
         {
             p1 = new Product("first", 10, "h", "g", 10, 10, 10);
@@ -27,7 +27,7 @@ namespace WorkshopProject.Tests
             shoppingCart.addProducts(p4, amount[3]);
         }
 
-        [TestCleanup]
+        //[TestCleanup]
         public void Cleanup()
         {
             shoppingCart = new ShoppingCart();
@@ -37,25 +37,34 @@ namespace WorkshopProject.Tests
         [TestCategory("Unit test - ShoppingCartTest")]
         public void SetProductAmountTest()
         {
-            //adding new item
-            int newAmount = 20;
-            Product testp = new Product("testp", 10, "Category.Categories.category1", "g", 10,10, 10);
-            shoppingCart.setProductAmount(testp, newAmount);
-            int inCartAmount = shoppingCart.getProducts()[testp];
-            Assert.AreEqual(newAmount, inCartAmount);
+            try
+            {
+                Init();
+                //adding new item
+                int newAmount = 20;
+                Product testp = new Product("testp", 10, "Category.Categories.category1", "g", 10, 10, 10);
+                shoppingCart.setProductAmount(testp, newAmount);
+                int inCartAmount = shoppingCart.getProducts()[testp];
+                Assert.AreEqual(newAmount, inCartAmount);
 
-            //mul amount 
-            shoppingCart.setProductAmount(testp, 2*newAmount);
-            Assert.AreEqual(newAmount*2, 2*inCartAmount);
+                //mul amount 
+                shoppingCart.setProductAmount(testp, 2 * newAmount);
+                Assert.AreEqual(newAmount * 2, 2 * inCartAmount);
 
-            //bad amount
-            Assert.IsFalse(shoppingCart.setProductAmount(p1, -20));
-            Assert.AreEqual(shoppingCart.getProductAmount(p1), amount[0]);
+                //bad amount
+                Assert.IsFalse(shoppingCart.setProductAmount(p1, -20));
+                Assert.AreEqual(shoppingCart.getProductAmount(p1), amount[0]);
 
-            //zero amount - remove product from list
-            Assert.IsTrue(shoppingCart.setProductAmount(testp, 0));
-            Assert.AreEqual(shoppingCart.getProductAmount(testp), 0);
-            Assert.IsFalse(shoppingCart.products.ContainsKey(testp));
+                //zero amount - remove product from list
+                Assert.IsTrue(shoppingCart.setProductAmount(testp, 0));
+                Assert.AreEqual(shoppingCart.getProductAmount(testp), 0);
+                Assert.IsFalse(shoppingCart.products.ContainsKey(testp));
+            }
+            finally
+            {
+                Cleanup();
+            }
+
         }
 
         
@@ -64,68 +73,90 @@ namespace WorkshopProject.Tests
         [TestCategory("Unit test - ShoppingCartTest")]
         public void addProductsTest()
         {
-            //adding new item
-            int newAmount = 20;
-            Product testp = new Product("testp", 10, "Category.Categories.category1","g", 10, 10, 10);
-            shoppingCart.addProducts(testp, newAmount);
-            int inCartAmount =shoppingCart.getProductAmount(testp);
-            Assert.AreEqual(newAmount, inCartAmount);
+            try
+            {
+                Init();
+                //adding new item
+                int newAmount = 20;
+                Product testp = new Product("testp", 10, "Category.Categories.category1", "g", 10, 10, 10);
+                shoppingCart.addProducts(testp, newAmount);
+                int inCartAmount = shoppingCart.getProductAmount(testp);
+                Assert.AreEqual(newAmount, inCartAmount);
 
-            //adding for old item
-            shoppingCart.addProducts(p1, newAmount);
-            inCartAmount = shoppingCart.getProductAmount(p1);
-            Assert.AreEqual(amount[0] + newAmount, inCartAmount);
+                //adding for old item
+                shoppingCart.addProducts(p1, newAmount);
+                inCartAmount = shoppingCart.getProductAmount(p1);
+                Assert.AreEqual(amount[0] + newAmount, inCartAmount);
 
-            shoppingCart.addProducts(p2, newAmount);
-            inCartAmount = shoppingCart.getProductAmount(p2);
-            Assert.AreEqual(amount[1] + newAmount, inCartAmount);
+                shoppingCart.addProducts(p2, newAmount);
+                inCartAmount = shoppingCart.getProductAmount(p2);
+                Assert.AreEqual(amount[1] + newAmount, inCartAmount);
 
-            //adding negativ number
-            Assert.IsFalse(shoppingCart.addProducts(p4, -20));
-            //item old amount 
-            Assert.AreEqual(shoppingCart.getProductAmount(p4), amount[3]);
+                //adding negativ number
+                Assert.IsFalse(shoppingCart.addProducts(p4, -20));
+                //item old amount 
+                Assert.AreEqual(shoppingCart.getProductAmount(p4), amount[3]);
+            }
+            finally
+            {
+                Cleanup();
+            }
+
         }
 
         [TestMethod()]
         [TestCategory("Unit test - ShoppingCartTest")]
         public void getProductAmountTest()
         {
-            //check matching amount
-            int pAmount = shoppingCart.getProductAmount(p1);
-            Assert.AreEqual(pAmount,amount[0]);
-            pAmount = shoppingCart.getProductAmount(p2);
-            Assert.AreEqual(pAmount, amount[1]);
-            pAmount = shoppingCart.getProductAmount(p3);
-            Assert.AreEqual(pAmount, amount[2]);
-            pAmount = shoppingCart.getProductAmount(p4);
-            Assert.AreEqual(pAmount, amount[3]);
+            try
+            {
+                Init();
+                //check matching amount
+                int pAmount = shoppingCart.getProductAmount(p1);
+                Assert.AreEqual(pAmount, amount[0]);
+                pAmount = shoppingCart.getProductAmount(p2);
+                Assert.AreEqual(pAmount, amount[1]);
+                pAmount = shoppingCart.getProductAmount(p3);
+                Assert.AreEqual(pAmount, amount[2]);
+                pAmount = shoppingCart.getProductAmount(p4);
+                Assert.AreEqual(pAmount, amount[3]);
 
-            //adding amount
-            int newAmount = 20;
-            Product testp = new Product("testp", 10, "Category.Categories.category1", "g", 10, 10, 10);
-            shoppingCart.setProductAmount(testp, newAmount);
-            pAmount = shoppingCart.getProductAmount(testp);
-            Assert.AreEqual(pAmount, newAmount);
-
+                //adding amount
+                int newAmount = 20;
+                Product testp = new Product("testp", 10, "Category.Categories.category1", "g", 10, 10, 10);
+                shoppingCart.setProductAmount(testp, newAmount);
+                pAmount = shoppingCart.getProductAmount(testp);
+                Assert.AreEqual(pAmount, newAmount);
+            }
+            finally
+            {
+                Cleanup();
+            }
         }
         
         [TestMethod()]
         [TestCategory("Unit test - ShoppingCartTest")]
         public void getTotalAmountTest()
         {
-            //defult amount
-            int currAmount = shoppingCart.getTotalAmount();
-            int goalAmmount = amount.Sum();
-            Assert.AreEqual(currAmount, goalAmmount);
+            try
+            {
+                Init();
+                //defult amount
+                int currAmount = shoppingCart.getTotalAmount();
+                int goalAmmount = amount.Sum();
+                Assert.AreEqual(currAmount, goalAmmount);
 
-            //adding new product 
-            Product newp = new Product("testp", 10, "Category.Categories.category1", "g", 10, 10, 10);
-            shoppingCart.addProducts(newp, 20);
-            goalAmmount += 20;
-            currAmount = shoppingCart.getTotalAmount();
-            Assert.AreEqual(currAmount, goalAmmount);
-            
-
+                //adding new product 
+                Product newp = new Product("testp", 10, "Category.Categories.category1", "g", 10, 10, 10);
+                shoppingCart.addProducts(newp, 20);
+                goalAmmount += 20;
+                currAmount = shoppingCart.getTotalAmount();
+                Assert.AreEqual(currAmount, goalAmmount);
+            }
+            finally
+            {
+                Cleanup();
+            }
         }
     }
 }
