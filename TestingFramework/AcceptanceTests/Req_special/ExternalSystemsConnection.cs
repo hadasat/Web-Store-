805d1b29cf80034913025cb399a0673a3255c031
Unknown <wolfj@post.bgu.ac.il>
2019-04-21 13:30:14 +0300

    ...

---
+++ TestingFramework/AcceptanceTests/Req_special/ExternalSystemsConnection.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingFramework.AcceptanceTests.Req_special
+{
+    
+    [TestClass]
+    public class ExternalSystemsConnection : AcceptanceTest
+    {
+
+        public override void Init()
+        {
+
+        }
+
+        public override void Cleanup()
+        {
+            
+        }
+
+
+        [TestMethod]
+        [TestCategory("Req_special")]
+        public void PaymentSystemConnectionTest()
+        {
+            try
+            {
+                Init();
+                PaymentSystemConnectionTestInner();
+            }
+            finally
+            {
+                Cleanup();
+            }
+        }
+
+        public void PaymentSystemConnectionTestInner()
+        {
+            //TODO: wait for external systems
+        }
+
+
+        [TestMethod]
+        [TestCategory("Req_special")]
+        public void SupplySystemConnectionTest()
+        {
+            try
+            {
+                Init();
+                SupplySystemConnectionTestInner();
+            }
+            finally
+            {
+                Cleanup();
+            }
+        }
+
+        public void SupplySystemConnectionTestInner()
+        {
+            //TODO: wait for external systems
+        }
+
+        [TestMethod]
+        [TestCategory("Req_special")]
+        public void ConsistencySystemConnectionTest()
+        {
+            try
+            {
+                Init();
+                SupplySystemConnectionTestInner();
+            }
+            finally
+            {
+                Cleanup();
+            }
+        }
+
+        public void ConsistencySystemConnectionTestInner()
+        {
+            //TODO: wait for external systems
+        }
+    }
+}
