01c201818e9fe5fa55ee8777c0cd6077f6c5bb14
Unknown <wolfj@post.bgu.ac.il>
2019-05-01 19:50:31 +0300

    ...

---
+++ WorkshopProject/Policies/ConcreteOutcome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users;
+
+namespace WorkshopProject.Policies
+{
+    public class FreeProduct : IOutcome
+    {
+        public FreeProduct()
+        {
+            //TODO FreeProduct
+        }
+
+        protected override List<ProductAmountPrice> myApply(List<ProductAmountPrice> products, User user)
+        {
+            //TODO FreeProduct
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class Percentage : IOutcome
+    {
+        public Percentage()
+        {
+            //TODO Percentage
+        }
+
+        protected override List<ProductAmountPrice> myApply(List<ProductAmountPrice> products, User user)
+        {
+            //TODO Percentage
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WorkshopProject/Policies/IOutcome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users;
+
+namespace WorkshopProject.Policies
+{
+    public abstract class IOutcome
+    {
+        protected IOutcome successor;
+
+        public List<ProductAmountPrice> Apply(List<ProductAmountPrice> products, User user)
+        {
+            List<ProductAmountPrice> modifiedList = myApply(products, user);
+            if(successor != null)
+            {
+                return successor.Apply(modifiedList, user);
+            }
+            else
+            {
+                return modifiedList;
+            }
+        }
+
+        public void AddSuccessor(IOutcome successor)
+        {
+            this.successor = successor;
+        }
+
+        protected abstract List<ProductAmountPrice> myApply(List<ProductAmountPrice> products, User user);
+    }
+}
