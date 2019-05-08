ed9213cb18d192cef63ceab3c55a174a9b6c9792
Unknown <wolfj@post.bgu.ac.il>
2019-05-08 17:24:14 +0300

    ...

---
+++ WorkshopProject/System Service/LoginProxy.cs
@@ -0,0 +1,274 @@
+﻿using Managment;
+using Shopping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users;
+
+namespace WorkshopProject.System_Service
+{
+    public class LoginProxy
+    {
+        bool loggedIn;
+        public User user { get; set; }
+
+        public LoginProxy()
+        {
+            user = new User();
+            loggedIn = false;
+        }
+
+        public void updateMember(User member)
+        {
+            user = member;
+        }
+
+        public string addDiscountPolicy(int storeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddProductToBasket(int storeId, int productId, int amount)
+        {
+            return TransactionService.AddProductToBasket(user, storeId, productId, amount);
+        }
+
+        public bool AddProductToStock(int storeId, int productId, int amount)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return StoreService.AddProductToStock(user, storeId, productId, amount);
+        }
+
+        public int AddProductToStore(int storeId, string name, string desc, double price, string category)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return StoreService.AddProductToStore(user, storeId, name, desc, price, category);
+        }
+
+        public bool addPurchasingPolicy(int storeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddStore(string storeName)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+
+            return StoreService.AddStore(user, storeName);
+
+        }
+
+        public bool AddStoreManager(int storeId, string userToAdd, string roles)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return UserService.AddStoreManager(user, storeId, userToAdd, roles);
+        }
+
+        public bool AddStoreOwner(int storeId, string userToAdd)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return UserService.AddStoreOwner(user, storeId, userToAdd);
+        }
+
+        public int BuyShoppingBasket()
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return TransactionService.BuyShoppingBasket(user);
+        }
+
+        public bool ChangeProductInfo(int storeId, int productId, string name, string desc, double price, string category, int amount)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return StoreService.ChangeProductInfo(user, storeId, productId, name, desc, price, category, amount);
+        }
+
+        public bool closeStore(int storeID)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return StoreService.CloseStore(user, storeID);
+        }
+
+        public Product GetProductInfo(int productId)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return StoreService.GetProductInfo(productId);
+        }
+
+        public JsonShoppingCart GetShoppingCart(int storeId)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return TransactionService.GetShoppingCart(user, storeId);
+        }
+
+        public JsonShoppingBasket GetShoppingBasket()
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return TransactionService.GetShoppingBasket(user);
+        }
+
+        public Member login(string username, string password)
+        {
+            Member ret;
+            ret = UserService.login(username, password);
+            if (ret != null)
+            {
+                updateMember(ret);
+                if (user is Member)
+                {
+                    loggedIn = true;
+                }
+                else
+                    loggedIn = false;
+            }
+            return ret;
+        }
+
+        public bool logout()
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            loggedIn = false;
+            bool ret;
+            ret = UserService.logout(user);
+            updateMember(new User());
+            return ret;
+        }
+
+        public bool Register(string username, string password)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return UserService.Register(username, password);
+        }
+        public bool Register(string username, string password, string country, int age)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return UserService.Register(username, password, country, age);
+        }
+
+        public bool removeDiscountPolicy(int storeId)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveProductFromStore(int storeId, int productId)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return StoreService.RemoveProductFromStore(user, storeId, productId);
+        }
+
+        public bool removePurchasingPolicy(int storeId)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveStoreManager(int storeId, string managerName)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return UserService.RemoveStoreManager(user, storeId, managerName);
+        }
+
+        public bool RemoveUser(string usernameToRemove)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return UserService.RemoveUser(user, usernameToRemove);
+        }
+
+        public List<Product> SearchProducts(string name, string category, string keyword, double startPrice, double endPrice, int productRank, int storeRank)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return StoreService.SearchProducts(name, category, keyword, startPrice, endPrice, productRank, storeRank);
+        }
+
+        public bool SetProductAmountInBasket(int storeId, int productId, int amount)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return TransactionService.SetProductAmountInBasket(user, storeId, productId, amount);
+        }
+
+        public Store GetStore(int storeId)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return StoreService.GetStore(storeId);
+        }
+
+        public List<Store> GetAllStores()
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return StoreService.GetAllStores();
+        }
+
+        public List<Member> GetAllManagers(int storeId)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return StoreService.getAllManagers(storeId);
+        }
+
+        public List<Member> GetAllOwners(int storeId)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return StoreService.getAllManagers(storeId);
+        }
+
+        public List<StoreManager> GetRoles()
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return UserService.GetRoles(user);
+        }
+
+        public List<Member> GetAllMembers()
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return UserService.GetAllMembers();
+        }
+
+        public bool SendMessage(int memberId, string message)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            UserService.SendMessage(memberId, message);
+            return true;
+        }
+
+        public List<string> GetMessages(int memberId)
+        {
+            if (!loggedIn)
+                notLoggedInException();
+            return UserService.GetMessages(memberId);
+        }
+
+
+        private void notLoggedInException()
+        {
+            throw new Exception("User not logged in");
+        }
+
+    }
+}
