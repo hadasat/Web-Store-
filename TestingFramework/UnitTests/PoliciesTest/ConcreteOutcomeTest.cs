9a6c0584ac233e70900849ed0809efa2233a325d
Unknown <atiyah@post.bgu.ac.il>
2019-05-10 13:36:56 +0300

    adding unitest class for policies

---
+++ TestingFramework/UnitTests/PoliciesTest/ConcreteOutcomeTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users;
+
+namespace WorkshopProject.Policies
+{
+    public class FreeProductTest
+    {
+        public void FreeProductApplyTest() { }
+    }
+
+
+    public class PercentageTest
+    {
+
+        public void PercentageApplyTest() { }
+
+    }
+
+
+}
---
+++ TestingFramework/UnitTests/PoliciesTest/DiscountTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users;
+
+namespace WorkshopProject.Policies
+{
+    public class DiscountTest
+    {
+        public void DiscountApplyTest()
+        {
+        }
+
+        public void checkDiscountTest()
+        {
+        }
+
+       
+    }
+}
