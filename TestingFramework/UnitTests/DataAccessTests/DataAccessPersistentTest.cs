d54ba398770756b929d961452cde64be17b599c4
Unknown <wolfj@post.bgu.ac.il>
2019-05-20 12:27:35 +0300

    testing begins

---
+++ TestingFramework/UnitTests/DataAccessTests/DataAccessPersistentTest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkshopProject.DataAccessLayer;
+
+namespace TestingFramework.UnitTests.DataAccessTests
+{
+    [TestClass]
+    public class DataAccessPersistentTest
+    {
+        DataAccessPersistent dal;
+
+        [TestInitialize]
+        public void Init()
+        {
+            dal = new DataAccessPersistent();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            //this method is called ONCE AFTER the test run
+        }
+
+        public void GetMemberTest(int id)
+        {
+
+        }
+
+        public void GetModeTest()
+        {
+
+        }
+
+        public void GetProductTest()
+        {
+
+        }
+
+        public void GetStoreTest()
+        {
+
+        }
+
+        public void SaveMemberTest()
+        {
+
+        }
+
+        public void SaveProductTest()
+        {
+
+        }
+        public void SaveStoreTest()
+        {
+
+        }
+
+        public void SetModeTest()
+        {
+
+        }
+    }
+}
---
+++ WorkshopProject/DataAccessLayer/DataAccessDriver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopProject.DataAccessLayer
+{
+    public static class DataAccessDriver
+    {
+        private static bool persistent = true;
+
+        public static IDataAccess GetDataAccess()
+        {
+            if (persistent)
+            {
+                return new DataAccessPersistent();
+            }
+            else
+            {
+                return new DataAccessNonPersistent();
+            }       
+        }
+    }
+}
