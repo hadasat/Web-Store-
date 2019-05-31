3e7d32f5adda93f2e98beca62edae4d1f21be4a1
Unknown <atiyah@post.bgu.ac.il>
2019-06-01 01:18:37 +0300

    init class initial

--- WorkshopProject/System Service/InitSystem.cs
+++ WorkshopProject/System Service/InitSystem.cs
@@ -8,14 +8,114 @@ namespace WorkshopProject.System_Service
 {
      public static class InitSystem
     {
-        public static string filePath = "../../../hadas.txt";
-        
+        private static string filePath = "../../../hadas.txt";
+        private static string commandSyntax = @"^[a-zA-Z0-9]+\([a-zA-Z0-9]*(,[[a-zA-Z0-9]+)*\);";
+
         public static void updatePath(string new_path) { filePath = new_path; }
+
+        public static string[] splitCommand(string sentence)
+        {
+            string word = @"\b[a-zA-Z1-9\-]+\b";
+            Regex rgxword = new Regex(word);
+            List<string> args = new List<string>();
+            foreach (Match match in rgxword.Matches(sentence))
+            {
+                args.Add(match.Value);
+            }
+            return args.ToArray();
+        }
 
         public static void initSystem()
         {
+           
             string[] lines = System.IO.File.ReadAllLines(@filePath);
+            if (lines == null)
+                return;
+            foreach(string line in lines)
+            {
+                Regex rgx = new Regex(commandSyntax);
+                if (rgx.IsMatch(line))
+                    break;
+                execute(line);
+            }
+        }
+
+        public static void execute(string line)
+        {
+            string[] fullcommand = splitCommand(line);
+            string command = fullcommand[0];
+            switch (command)
+            {
+                case "register":
+                    registerNewUser(fullcommand);
+                    break;
+                case "appoint-manager":
+                    appointManager(fullcommand);
+                    break;
+                case "open-store":
+                    openStore(fullcommand);
+                    break;
+                case "add-product-basket":
+                    addProductBasket(fullcommand);
+                    break;
+                case "add-product-stock":
+                    addProductStock(fullcommand);
+                    break;
+                case "add-product-store":
+                    addProductStore(fullcommand);
+                    break;
+                case "add-store-owner":
+                    addStoreOwner(fullcommand);
+                    break;
+                case "login-user":
+                    loginuser(fullcommand);
+                    break;
+
+            }
+        }
+
+        public static void registerNewUser(string[] fullcommand)
+        {
 
         }
+
+        public static void appointManager(string[] fullcommand)
+        {
+
+        }
+
+        public static void openStore(string[] fullcommand)
+        {
+
+        }
+        public static void addProductBasket(string[] fullcommand)
+        {
+
+        }
+        public static void addProductStock(string[] fullcommand)
+        {
+
+        }
+
+        public static void addProductStore(string[] fullcommand)
+        {
+
+        }
+
+        public static void addStoreOwner(string[] fullcommand)
+        {
+
+        }
+
+        public static void appointManager(string[] fullcommand)
+        {
+
+        }
+
+        public static void loginuser(string[] fullcommand)
+        {
+
+        }
+        
     }
 }
