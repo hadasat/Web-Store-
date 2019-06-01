adc77e8587994c7565e89c9b66314c675ae9fb9d
Unknown <wolfj@post.bgu.ac.il>
2019-06-01 18:32:36 +0300

    ...

---
+++ WorkshopProject/DataAccessLayer/Repo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopProject.DataAccessLayer
+{
+    public class Repo
+    {
+        private WorkshopDBContext ctx;
+
+        public Repo()
+        {
+            ctx = new WorkshopTestDBContext();
+        }
+
+        public IEntity Get<T>() where T : IEntity
+        {
+            return null;
+
+
+
+        }
+
+        public void Save<T>() where T : IEntity {
+
+        }
+
+        public void Remove<T>() where T : IEntity
+        {
+
+        }
+
+        public void SaveChanges()
+        {
+
+        }
+
+    }
+}
