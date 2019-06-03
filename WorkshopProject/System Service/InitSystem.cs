0743861d7b311672697d265adfd0baca237a113c
Unknown <atiyah@post.bgu.ac.il>
2019-06-03 18:23:05 +0300

    init system

--- WorkshopProject/System Service/InitSystem.cs
+++ WorkshopProject/System Service/InitSystem.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Managment;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Users;
 
 namespace WorkshopProject.System_Service
 {
@@ -12,6 +15,8 @@ namespace WorkshopProject.System_Service
         private static string word = @"\b[a-zA-Z1-9\-]+\b";
         private static string commandSyntax = @"^[a-zA-Z0-9\-]+\([a-zA-Z0-9]*(,[[a-zA-Z0-9]+)*\);";
 
+        private static Dictionary<string, string> users;
+
         public static void updatePath(string new_path) { filePath = new_path; }
 
         public static string[] splitCommand(string sentence)
@@ -19,15 +24,12 @@ namespace WorkshopProject.System_Service
             Regex rgxword = new Regex(word);
             List<string> args = new List<string>();
             foreach (Match match in rgxword.Matches(sentence))
-            {
-                args.Add(match.Value);
-            }
+            args.Add(match.Value);
             return args.ToArray();
         }
 
         public static void initSystem()
         {
-           
             string[] lines = System.IO.File.ReadAllLines(@filePath);
             if (lines == null)
                 return;
@@ -50,16 +52,13 @@ namespace WorkshopProject.System_Service
                     registerNewUser(fullcommand);
                     break;
                 case "make-admin":
-                    appointManager(fullcommand);
+                    makeAdmin(fullcommand);
                     break;
                 case "open-store":
                     openStore(fullcommand);
                     break;
                 case "add-product-basket":
                     addProductBasket(fullcommand);
-                    break;
-                case "add-product-stock":
-                    addProductStock(fullcommand);
                     break;
                 case "add-store-product":
                     addProductStore(fullcommand);
@@ -70,13 +69,38 @@ namespace WorkshopProject.System_Service
                 case "login-user":
                     loginuser(fullcommand);
                     break;
-
             }
+        }
+
+        public static User getUser(string username, string password)
+        {
+            int userId = ConnectionStubTemp.identifyUser(username, password);
+            User user = ConnectionStubTemp.getMember(userId);
+            return user;
+        }
+
+        public static Store getStore(string name)
+        {
+            foreach(KeyValuePair<int,Store> s in WorkShop.stores)
+            {
+                Store store = s.Value;
+                if (store.name.Equals(name))
+                    return store;
+            }
+            return null;
         }
 
         public static void registerNewUser(string[] fullcommand)
         {
-
+            if (fullcommand.Length == 4) {
+                string username = fullcommand[0];
+                string password = fullcommand[1];
+                string country = fullcommand[2];
+                string stringBirthdate = fullcommand[2];
+                DateTime birthdate = DateTime.Parse(stringBirthdate);
+                if (UserService.Register(username, password, birthdate, country))
+                    users.Add(username, password);
+            }
         }
 
         public static void makeAdmin(string[] fullcommand)
@@ -86,32 +110,72 @@ namespace WorkshopProject.System_Service
 
         public static void openStore(string[] fullcommand)
         {
-
+            if (fullcommand.Length == 2)
+            {
+                string username = fullcommand[0];
+                string storeName = fullcommand[1];
+                try
+                {
+                    User user = getUser(username, users[username]);
+                    StoreService.AddStore(user, storeName);
+                }
+                catch { }
+            }
         }
         public static void addProductBasket(string[] fullcommand)
-        {
-
-        }
-        public static void addProductStock(string[] fullcommand)
         {
 
         }
 
         public static void addProductStore(string[] fullcommand)
         {
+            if (fullcommand.Length == 7)
+            {
+                string username = fullcommand[0];
+                string storeName = fullcommand[1];
+                string productName = fullcommand[2];
+                string category = fullcommand[3];
+                string desc = fullcommand[4];
+                int price = int.Parse(fullcommand[5]);
+                int amount = int.Parse(fullcommand[6]);
+                try
+                {
+                    User user = getUser(username, users[username]);
+                    Store store = getStore(storeName);
+                    int storeId = store.id;
+                    int productId = StoreService.AddProductToStore(user, storeId, productName, desc, price, category);
 
+                    StoreService.AddProductToStock(user, storeId, productId, amount);
+                }
+                catch { }
+            }
+            
         }
 
         public static void addStoreOwner(string[] fullcommand)
         {
 
+            if (fullcommand.Length == 3)
+            {
+                string username = fullcommand[0];
+                string storeName = fullcommand[1];
+                string newManager = fullcommand[2];
+                
+                try
+                {
+                    User user = getUser(username, users[username]);
+                    Store store = getStore(storeName);
+                    int storeId = store.id;
+
+                    Roles roles = new Roles(true, true, true, false, false, false, false, false, true);
+                    string sushiRole = JsonHandler.SerializeObject(roles);
+
+                    UserService.AddStoreManager(user, storeId, newManager, sushiRole);
+                }
+                catch { }
+            }
         }
-
-        public static void appointManager(string[] fullcommand)
-        {
-
-        }
-
+        
         public static void loginuser(string[] fullcommand)
         {
 
