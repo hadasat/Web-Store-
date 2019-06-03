44826767330ef3be7ff95023f1a179153d2f3590
arielAmselBGU <amsel@post.bgu.ac.il>
2019-06-03 20:31:52 +0300

    navbar test for all pages

--- TestingFramework/GUI/cypress/integration/home.js
+++ TestingFramework/GUI/cypress/integration/home.js
@@ -10,25 +10,4 @@ describe('The Home Page', function() {
     cy.get('h1').should('contain', 'Stores')
    
   })
-
-  it('nav bar exists', function() {
-    cy.visit('http://localhost:8080/wot')
-    cy.get('#navbar_header').should('be.visible')
-	// todo copy to all webpages and kill ofir
-  })
-  
-  it('nav bar for guests', function() {
-    cy.visit('http://localhost:8080/wot')
-    cy.get('#navbar_header').get('#nav_signin').should('be.visible')
-	cy.get('#navbar_header').get('#nav_advanced_search').should('be.visible')
-	cy.get('#navbar_header').get('#nav_shopping_basket').should('be.visible')
-	cy.get('.navbar-brand').should('be.visible')
-	cy.get('#navbar_header').get('#nav_userName').should('not.be.visible')
-	cy.get('#navbar_header').get('#nav_signOut').should('not.be.visible')
-	cy.get('#navbar_header').get('#nav_opensStore').should('not.be.visible')
-	cy.get('#navbar_header').get('#nav_admin').should('not.be.visible')
-	// todo copy to all webpages and kill ofir
-  })
-  
-  
 })
