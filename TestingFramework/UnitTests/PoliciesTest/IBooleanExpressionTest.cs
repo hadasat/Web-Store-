e7ade69f53a5cb59b716537d28a82e10dda14ee6
Unknown <atiyah@post.bgu.ac.il>
2019-06-08 00:41:36 +0300

    testing consistency

--- TestingFramework/UnitTests/PoliciesTest/IBooleanExpressionTest.cs
+++ TestingFramework/UnitTests/PoliciesTest/IBooleanExpressionTest.cs
@@ -21,12 +21,38 @@ namespace WorkshopProject.Policies
     {
         [TestMethod]
         [TestCategory("POLICIES")]
-
         public void checkExpressionTest() {
             int currId = IBooleanExpression.Idcounter;
             IBooleanExpression.checkExpression(new TrueCondition());
             int newId = IBooleanExpression.Idcounter;
             Assert.IsTrue(currId < newId);
+        }
+
+        [TestMethod]
+        [TestCategory("POLICIES")]
+        public void checkConsistency()
+        {
+            int min = 10, max = 5;
+            TrueCondition trueCondition = new TrueCondition();
+            FalseCondition falseCondition = new FalseCondition();
+            Assert.IsFalse(trueCondition.checkConsistent(falseCondition),"1");
+            Assert.IsFalse(falseCondition.checkConsistent(trueCondition),"2");
+
+            MinAmount minAmount = new MinAmount(min, new AllProductsFilter());
+            MaxAmount maxAmount = new MaxAmount(max, new AllProductsFilter());
+            Assert.IsFalse(minAmount.checkConsistent(maxAmount),"3");
+            Assert.IsFalse(maxAmount.checkConsistent(minAmount),"4");
+
+            List<IBooleanExpression> list = new List<IBooleanExpression>();
+            list.Add(trueCondition);
+            list.Add(minAmount);
+
+            Assert.IsFalse(IBooleanExpression.confirmListConsist(falseCondition, list));
+            Assert.IsFalse(IBooleanExpression.confirmListConsist(maxAmount, list));
+
+            minAmount.amount = max;
+            maxAmount.amount = min;
+            Assert.IsTrue(minAmount.checkConsistent(maxAmount), "5");
         }
 
     }
