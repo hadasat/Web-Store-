55a3ceb0e3939ce143a8d1ca6b847cef114ab53d
Unknown <wolfj@post.bgu.ac.il>
2019-06-11 14:55:17 +0300

    added db win for Repo, hopefully nothing breaks

--- WorkshopProject/DataAccessLayer/Repo.cs
+++ WorkshopProject/DataAccessLayer/Repo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,9 @@ using Users;
 namespace WorkshopProject.DataAccessLayer
 {
     //https://blog.goyello.com/2011/11/23/entity-framework-invalid-operation/
+
+    //https://docs.microsoft.com/en-us/ef/ef6/saving/concurrency
+
     public class Repo
     {
         //private static WorkshopDBContext ctx;
@@ -47,12 +51,14 @@ namespace WorkshopProject.DataAccessLayer
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="entity"></param>
+ 
+        /*
         public virtual void Add<T>(List<T> entity) where T : IEntity
         {
             WorkshopDBContext ctx = getContext();
             ctx.Set<T>().AddRange(entity);
             SaveChanges();
-        }
+        }*/
 
         public virtual void Add<T>(T entity) where T : IEntity
         {
@@ -67,11 +73,13 @@ namespace WorkshopProject.DataAccessLayer
             ctx.Set<T>().Remove(entity);
         }
 
+        /*
         public virtual void Remove<T>(List<T> entity) where T : IEntity
         {
             WorkshopDBContext ctx = getContext();
             ctx.Set<T>().RemoveRange(entity);
         }
+        */
 
         //public void Clear<T>() where T : IEntity
         //{
@@ -111,7 +119,29 @@ namespace WorkshopProject.DataAccessLayer
         {
             WorkshopDBContext ctx = getContext();
             ctx.ChangeTracker.DetectChanges();
-            ctx.SaveChanges();
+
+            //use database wins
+            bool saveFailed;
+            do
+            {
+                saveFailed = false;
+
+                try
+                {
+                    ctx.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    saveFailed = true;
+
+                    // Update the values of the entity that failed to save from the store
+                    ex.Entries.Single().Reload();
+                }
+
+            } while (saveFailed);
+
+
+           // ctx.SaveChanges();
         }
 
         //https://www.axian.com/2015/06/24/entity-framework-6-taming-the-open-connection-conundrum/
